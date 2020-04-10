21d47cc6018421235546a7891382061213703781
a <2573263498@qq.com>
2020-04-10 16:34:21 +0800

    20200410

---
+++ VirDir.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Expt1Site.SubDir
+{
+    public partial class VirDir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lb1Msg.Text = "虚拟测试目录！";
+        }
+    }
+}
