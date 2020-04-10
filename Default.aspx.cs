681b4e72caa0b99f3d950dac3215a6386239ec0e
a <2573263498@qq.com>
2020-04-10 16:33:10 +0800

    20200410

---
+++ Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Expt1Site
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lb1Msg.Text = "欢迎进入我的第一个网页，我开始学习ASP.NET了！";
+        }
+    }
+}
