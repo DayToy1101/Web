86b4e25c2ce5e88b907ca5e9b023f15a7893a2e2
a <2573263498@qq.com>
2020-05-03 22:37:45 +0800

    20200503

---
+++ DataManage.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DataManage : System.Web.UI.Page
+{
+    MyPetShopDataContext db = new MyPetShopDataContext();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Bind()
+    {
+        var results = from c in db.Category select c;
+        gvCategory.DataSource = results;
+        gvCategory.DataBind();
+    }
+    protected void btnQueryAll_Click(object sender, EventArgs e)
+    {
+        Bind();
+    }
+    protected void btnFuzzy_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("FuzzyQuery.aspx");
+    }
+    protected void btnInsert_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Insert.aspx");
+    }
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Update.aspx?CategoryId="+txtCategoryId.Text);
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        var results = from c in db.Category where c.CategoryId == int.Parse(txtCategoryId.Text) select c;
+        db.Category.DeleteAllOnSubmit(results);
+        Bind();
+    }
+}
