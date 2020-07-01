cd336e0fe283c66f67ddbd685f9f47f8a8589925
a <2573263498@qq.com>
2020-07-01 22:21:05 +0800

    20200701

---
+++ Leaver_kind.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+public partial class WebFiles_Leaver_Leaver_kind : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            Bind();
+    }
+    private void Bind()
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Mispersonalconn"].ConnectionString);
+        string sql = "select L_ID 类型编号,L_Kind 请假类型,L_IsSalary_Not 是否带薪 from [Tb_leaver_kind]";
+        SqlDataAdapter sda = new SqlDataAdapter(sql, con);
+        DataSet ds = new DataSet();
+        sda.Fill(ds, "temp");
+        con.Close();
+        ListLeaverKind.DataSource = ds.Tables["temp"].DefaultView;
+        ListLeaverKind.DataBind();
+    }
+}
